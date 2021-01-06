4911311e125efda486069646df472c4f1bed0b98
AndyHcodes <67926150+AndyHcodes@users.noreply.github.com>
2021-01-06 20:40:30 +0000

    fixed unmerged files

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
-import React from 'react';
-import Fixtures from './Fixtures';
-
-function App() {
-  return (
-    <div className="App">
-      Last Person Standing
-      <Fixtures />
-    </div>
-=======
-import '../App.css';
-import Login from './Login';
-import Register from './Register';
-import MemberArea from './MembersArea';
+import React from "react";
+import Fixtures from "./Fixtures";
+import "../App.css";
+import Login from "./Login";
+import Register from "./Register";
+import MemberArea from "./MembersArea";
 
 function App() {
   return (
     <>
-    <Login />
-    <Register />
-    <MemberArea />
+      Last Person Standing
+      <Login />
+      <Register />
+      <MemberArea />
+      <Fixtures />
     </>
->>>>>>> 7a4dae46b86252b6ff755581bb5115fbc79a3398
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
-import './index.css';
->>>>>>> 7a4dae46b86252b6ff755581bb5115fbc79a3398
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
